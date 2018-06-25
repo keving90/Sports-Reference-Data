cb678afb5670eef1b41a64351eb01c4a33fda96b
keving90 <kevingeiszler90@gmail.com>
2018-06-24 18:58:23 -0700

    Add more documentation

--- player.py
+++ player.py
@@ -2,6 +2,8 @@
 
 """
 This module contains a Player class used to represent an NFL player. Its attributes are various statistics.
+
+Note: This module was built using Python 3.6.1, so dictionaries are ordered.
 """
 
 import numpy as np
@@ -11,11 +13,24 @@ from datetime import datetime
 class Player(object):
     """
 
-    Represent a record in the data set. Uses a dictionary to assign attributes and give them appropriate data
-    types. The dictionary's keys will be class attributes. The dictionary's value will be attribute data types.
+    Represents a record in the data set.
+
+    Attributes:
+        Uses a dictionary to assign attributes and give them appropriate data types. The dictionary's keys will be
+        class attributes. The dictionary's value will be attribute data types.
 
     """
     def __init__(self, data, header):
+        """
+        Initialize Player object.
+
+        Args:
+            data (list): List of player stats. Order of stats must match order of keys in header dict.
+
+            header (dict): Dictionary whose keys are the stats name and values are a data type. The keys will be used
+                as attribute names, and the values will be the data type of the attribute. The elements in the data
+                list will be the values of each attribute.
+        """
         # Loop through the header dictionary keys and values. An enumeration is used to grab data from a specific
         # column in the row. Use the keys and values in header to assign attributes and data types for
         # the attributes.
@@ -36,6 +51,7 @@ class Player(object):
                 continue
 
             # Remove unwanted characters in data.
+            # These qualities are found when scraping from www.pro-football-reference.com.
             # '*' in the player's name indicates a Pro Bowl appearance.
             # '+' in the player's name indicates a First-Team All-Pro award.
             # '%' is included in the catch percentage stat on pro-football reference
