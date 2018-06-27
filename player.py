e4472642ed6b3cc66d2c45b34efa2b2fe90ec0fa
keving90 <kevingeiszler90@gmail.com>
2018-06-27 11:10:27 -0700

    Use zip() to iterate instead of enumerate()

--- player.py
+++ player.py
@@ -20,33 +20,34 @@ class Player(object):
         class attributes. The dictionary's value will be attribute data types.
 
     """
-    def __init__(self, data, header):
+    def __init__(self, data, attr_dict):
         """
         Initialize Player object.
 
         Args:
-            data (list): List of player stats. Order of stats must match order of keys in header dict.
+            data (list): List of player stats. Order of stats must match order of keys in attr_dict dict. The elements
+                in the data list will be the values of each attribute.
 
-            header (dict): Dictionary whose keys are the stats name and values are a data type. The keys will be used
-                as attribute names, and the values will be the data type of the attribute. The elements in the data
-                list will be the values of each attribute.
+            attr_dict (dict): Dictionary whose keys are the stat names and values are a data type. The keys will be
+                used as attribute names for the object. The values will be the data type of the attribute.
         """
-        # Loop through the header dictionary keys and values. An enumeration is used to grab data from a specific
-        # column in the row. Use the keys and values in header to assign attributes and data types for
-        # the attributes.
-        for i, (attr, data_type) in enumerate(header.items()):
+        # Simultaneously loop through each stat, attribute name, and data type of each attribute.
+        # Modify the stat if needed. Assign each attribute using setattr().
+        for stat, (attr, data_type) in zip(data, attr_dict.items()):
             # In a player's game log table, if '@' is in the row, then it was an away game.
             # Otherwise, it was a home game.
+            # This situation arises when scraping data from www.pro-football-reference.com.
             if attr == 'location':
-                if data[i] == '@':
+                if stat == '@':
                     setattr(self, attr, 'away')
                 else:
                     setattr(self, attr, 'home')
                 continue
 
             # If data type is datetime, then convert data to type datetime.
+            # Dates are present in a www.pro-football-reference.com game log.
             if data_type == datetime:
-                date = datetime.strptime(data[i], '%Y-%m-%d').date()
+                date = datetime.strptime(stat, '%Y-%m-%d').date()
                 setattr(self, attr, date)
                 continue
 
@@ -55,18 +56,18 @@ class Player(object):
             # '*' in the player's name indicates a Pro Bowl appearance.
             # '+' in the player's name indicates a First-Team All-Pro award.
             # '%' is included in the catch percentage stat on pro-football reference
-            if data[i].endswith('*+'):
-                data[i] = data[i][:-2]
-            elif data[i].endswith('%') or data[i].endswith('*') or data[i].endswith('+'):
-                data[i] = data[i][:-1]
+            if stat.endswith('*+'):
+                stat = stat[:-2]
+            elif stat.endswith('%') or stat.endswith('*') or stat.endswith('+'):
+                stat = stat[:-1]
 
             # Set the class attribute. If the data is empty then we will assign a NumPy NaN value to the attribute.
             # Otherwise, we set the attribute as usual.
-            if not data[i]:
+            if not stat:
                 setattr(self, attr, np.NaN)
             else:
-                if ',' in data[i]:
-                    no_comma = ''.join(data[i].split(','))
+                if ',' in stat:
+                    no_comma = ''.join(stat.split(','))
                     setattr(self, attr, data_type(no_comma))
                 else:
-                    setattr(self, attr, data_type(data[i]))
+                    setattr(self, attr, data_type(stat))
