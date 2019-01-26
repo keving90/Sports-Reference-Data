54cc9c5fdced9d68b0df0e4b81655901d9c3fd73
keving90 <kevingeiszler90@gmail.com>
2019-01-26 10:54:04 -0800

    Update comments

--- pro_football_ref/pro_ref2.py
+++ pro_football_ref/pro_ref2.py
@@ -1,10 +1,27 @@
+"""
+This module contains a ProFbRefScraper class used to scrape NFL data from www.pro-football-reference.com. It places
+the data into a Pandas data frame and saves it as a CSV file. Built using Python 3.7.0.
+"""
+
 import requests
 import bs4
-import numpy as np
 import pandas as pd
 
 
-class ProRefScraper(object):
+class ProFbRefScraper(object):
+    """
+    Scrapes NFL data from www.pro-football-reference.com and places it into a Pandas data frame. Multiple years of data
+    can be scraped and placed into a single data frame for the same statistical category. These categories include:
+
+    'rushing': Rushing data.
+    'passing': Passing data.
+    'receiving': Receiving data.
+    'kicking': Field goal, point after touchdown, and punt data.
+    'returns': Punt and kick return data.
+    'scoring': All types of scoring data, such as touchdowns (defense/offense), two point conversions, kicking, etc.
+    'fantasy': Rushing, receiving, and passing stats, along with fantasy point totals from various leagues.
+    'defense': Defensive player stats.
+    """
     def __init__(self):
         self._tables = ['rushing', 'passing', 'receiving', 'kicking', 'returns', 'scoring', 'fantasy', 'defense']
         self._possible_cols_for_all_seasons = None
@@ -26,33 +43,14 @@ class ProRefScraper(object):
         """getter: Returns a list of the possible table types to scrape from."""
         return self._tables
 
-    """
-    check table type
-    check years
-    
-    if start == end:
-        get single season
-    else:
-        get multiple seasons
-        
-    get_single_season:
-        keep as is, but should not worry about columns for multiple seasons
-        
-    get multiple seasons:
-        uses get_single_season, but keeps track of cols for all seasons (outside of get_single_season)
-        can use logic from current get_data function
-    """
-
     def get_data(self, start_year, end_year, table_type, remove_pro_bowl=True, remove_all_pro=True):
         """
-        Gets a data frame of NFL player stats for one for more seasons.
-
+        Gets a data frame of NFL player stats for one for more seasons from Pro Football Reference.
         :param start_year: First season to scrape data from (string or int)
         :param end_year: Final season (inclusive) to scrape data from (string or int)
         :param table_type: Stat category to scrape
-        :param remove_pro_bowl:
-        :param remove_all_pro:
-
+        :param remove_pro_bowl: Boolean - If true, removes Pro Bowl accolade ('*') from player's name
+        :param remove_all_pro: Boolean - If true, removes All-Pro accolade ('+') from player's name
         :return: Data frame of one or more seasons of data for a given stat.
         """
         self._check_table_type(table_type)
@@ -78,37 +76,36 @@ class ProRefScraper(object):
         return df
 
     def _get_multiple_seasons(self, start_year, end_year, table_type):
+        """
+        Scrapes multiple seasons of data from Pro Football Reference and puts it into a Pandas data frame.
+        :param start_year: First season to scrape data from (string or int)
+        :param end_year: Final season (inclusive) to scrape data from (string or int)
+        :param table_type: Stat category to scrape
+        :return: Data frame with multiple seasons of data for a given stat category.
+        """
         # Get seasons to iterate through.
         year_range = self._get_year_range(start_year, end_year)
 
-        data_frames = []
+        seasons = []
 
         # Get individual df for each season.
         for year in year_range:
             single_season_df = self._get_single_season(year, table_type)
-            data_frames.append(single_season_df)
+            seasons.append(single_season_df)
             self._add_possible_cols_for_all_seasons(list(single_season_df.columns))
 
         # Combine all seasons into one large df.
         # sort = False prevents FutureWarning when concatenating data frames with different number of columns (1/18/19)
-        big_df = pd.concat(data_frames, sort=False)
-
-        # if self._possible_cols_for_all_seasons[-1] != 'year':
-        #     self._possible_cols_for_all_seasons.remove('year')
-        #     self._possible_cols_for_all_seasons.append('year')
-        #     # Reorder columns. Need to create a deep copy in order to prevent a SettingWithCopy warning.
-        #     big_df = big_df[self._possible_cols_for_all_seasons].copy()
+        big_df = pd.concat(seasons, sort=False)
 
         return big_df
 
     def _get_year_range(self, start_year, end_year):
         """
-        Uses a start_year and end_year to build a range iterator.
-
-        :param start_year: Year to begin iterator at.
-        :param end_year: Year to end iterator at.
-
-        :return: A range iterator.
+        Uses start_year and end_year to build an iterable sequence.
+        :param start_year: Year to begin iterable at.
+        :param end_year: Final year in iterable.
+        :return: An iterable sequence.
         """
         # Build range iterator depending on how start_year and end_year are related.
         if start_year > end_year:
@@ -119,6 +116,12 @@ class ProRefScraper(object):
         return year_range
 
     def _check_start_and_end_years(self, start_year, end_year):
+        """
+        Tries to convert start_year and end_year to int, if necessary. Raises ValueError for unsuccessful conversions.
+        :param start_year: Data to convert to int
+        :param end_year: Data to convert to int
+        :return: Tuple - (start_year, end_year)
+        """
         # Convert years to int, if needed.
         if not isinstance(start_year, int):
             try:
@@ -135,28 +138,26 @@ class ProRefScraper(object):
 
     def _get_single_season(self, year, table_type):
         """
-        Scrapes a table from Pro Football Reference based on the table_type and puts it into a Pandas data frame.
-
+        Scrapes a single table from Pro Football Reference and puts it into a Pandas data frame.
         :param year: Season's year.
         :param table_type: String representing the type of table to be scraped.
-
         :return: A data frame of the scraped table for a single season.
         """
-
         table_rows = self._get_table(year, table_type)
         header_row = self._get_table_headers(table_rows)
-        df_cols = self._get_df_columns(header_row, table_type)
-        player_elements = self._get_player_elements(table_rows)
+        df_cols = self._get_df_columns(header_row)
+        player_elements = self._get_player_rows(table_rows)
         season_data = self._get_player_stats(player_elements)
 
-        # final data frame for single season
-        return self._make_df(year, season_data, df_cols, table_type)
+        # Final data frame for single season
+        return self._make_df(year, season_data, df_cols)
 
     def _get_table(self, year, table_type):
         """
-        PROVIDE
-
-        :return: PROVIDE
+        Sends a GET request to Pro-Football Reference and uses BeautifulSoup to find the HTML table.
+        :param year: Season's year.
+        :param table_type: String representing the type of table to be scraped.
+        :return: BeautifulSoup table element.
         """
         # Send a GET request to Pro-Football Reference
         url = 'https://www.pro-football-reference.com/years/' + str(year) + '/' + table_type + '.htm'
@@ -169,16 +170,17 @@ class ProRefScraper(object):
         table = soup.find('table', id=table_type)
 
         if table is None:
-            raise AttributeError("Table for '" + table_type + "' stats not found for year " + str(year) + ".")
+            # No table found
+            raise AttributeError(table_type.capitalize() + " stats table not found for year " + str(year) + ".")
 
         # Return the table containing the data.
         return table
 
     def _get_table_headers(self, table_element):
         """
-
-        :param table_element:
-        :return:
+        Extracts the top row of a BeautifulSoup table element.
+        :param table_element: BeautifulSoup table element.
+        :return: List of header cells from a table.
         """
         # 'thead' contains the table's header row
         head = table_element.find('thead')
@@ -189,11 +191,11 @@ class ProRefScraper(object):
         # 'th' is a table header cell
         return col_names.find_all('th')
 
-    def _get_df_columns(self, header_elements, table_type):
+    def _get_df_columns(self, header_elements):
         """
-
-        :param header_elements:
-        :return:
+        Extracts stat names from column header cells.
+        :param header_elements: List of header cells
+        :return: List of stat names.
         """
         cols_for_single_season = [header_cell['data-stat'] for header_cell in header_elements[1:]]
         cols_for_single_season.insert(1, 'player_url')
@@ -202,85 +204,60 @@ class ProRefScraper(object):
 
     def _add_possible_cols_for_all_seasons(self, single_season_cols):
         """
-
+        Keeps track of all stat column names that will be used when scraping multiple seasons. Different seasons can
+        have different stat columns for a given category.
         :param single_season_cols:
         :return:
         """
         if not self._possible_cols_for_all_seasons:
+            # No columns accounted for yet.
             self._possible_cols_for_all_seasons = single_season_cols
         else:
+            # Check for column names that haven't been accounted for.
             new_cols = list(set(single_season_cols) - set(self._possible_cols_for_all_seasons))
             self._possible_cols_for_all_seasons += new_cols
 
-    def _get_table_rows(self, table_element):
+    def _get_player_rows(self, table_element):
         """
-
-        :param table_element:
-        :return:
+        Gets a list of rows from an HTML table.
+        :param table_element: HTML table.
+        :return: A list of table row elements.
         """
-        # tbody is the table's body
-        # Get the body of the table
+        # 'tbody' is the table's body
         body = table_element.find('tbody')
 
-        # tr refers to a table row
-        # Each element in player_list has data for a single player.
-        # This will also collect descriptions of each column found in the web page's table, which
-        # is filtered out in create_player_objects().
-        return body.find_all('tr')
-
-    def _get_player_elements(self, table_element):
-        # tbody is the table's body
-        # Get the body of the table
-        body = table_element.find('tbody')
-
-        # tr refers to a table row
-        # Each element in player_list has data for a single player.
-        # This will also collect descriptions of each column found in the web page's table, which
-        # is filtered out in create_player_objects().
+        # 'tr' refers to a table row
         return body.find_all('tr')
 
     def _get_player_stats(self, player_row_elements):
         """
-
-        :param player_row_elements:
-        :return:
+        Gets stats for each player in a table for a season.
+        :param player_row_elements: List of table rows where each row is a player's season stat line.
+        :return: List where each element is a list containing a player's data for the season.
         """
-        # This list holds a dictionary of each object's attributes.
-        # Each dictionary is made from the object's __dict__ attribute.
-        league_stats = []
-
-        # Get each player's stats, create a Player object, and append the object
-        # and the instance's __dict__ to their own list.
+        season_stats = []
         for player in player_row_elements:
-            # The <td> tag defines a standard cell in an HTML table.
-            # Get a list of cells. This raw web page data represents one player.
-            stats = player.find_all('td')
+            # 'td' is an HTML table cell
+            player_stats = player.find_all('td')
 
-            # If raw_stat_list has data, then we will extract the desired information from the elements.
-            # info_list will be empty if the current 'player' in the player_list is actually other
-            # irrelevant information we're not interested in (such as a column description).
-            if stats:
-                # Create a Player object and append the __dict__ attribute to a list.
-                # This list is used for the data in our data frame.
-                clean_stats = self._get_clean_stats(stats)
-                league_stats.append(clean_stats)
+            # Some rows do not contain player data.
+            if player_stats:
+                clean_stats = self._get_clean_stats(player_stats)
+                season_stats.append(clean_stats)
 
-        return league_stats
+        return season_stats
 
     def _get_clean_stats(self, stat_row):
         """
-        Gets clean text stats from a list of BeautifulSoup4 element tags.
-
-        :param raw_stat_list: List of BeautifulSoup4 element ResultSets. Inside of each ResultSet is a stat.
-
-        :return: List of the player's stats as strings.
+        Gets clean text stats for a player's season.
+        :param stat_row: List of table cells representing a player's stat line for a season.
+        :return: List of strings representing a player's season stat line.
         """
         clean_player_stats = []
         for stat_cell in stat_row:
             clean_player_stats.append(stat_cell.text)
 
-            # Every tag has an attribute.
-            # If the tag's data-stat attribute is 'player', then we get the player's URL.
+            # Also grab the player's URL so they have a unique identifier when combined with the season's year.
             if stat_cell['data-stat'] == 'player':
                 url = self._get_player_url(stat_cell)
                 clean_player_stats.append(url)
@@ -289,33 +266,25 @@ class ProRefScraper(object):
 
     def _get_player_url(self, player_cell):
         """
-
-        :param player:
-        :return:
+        Get's a player's unique URL.
+        :param player_cell: HTML table cell.
+        :return: String - player's unique URL.
         """
-        # href specifies the URL of the page the link goes to.
+        # 'href' is the URL of the page the link goes to.
         href = player_cell.find_all('a', href=True)
 
         # Return URL string
         return href[0]['href']
 
-    def _make_df(self, year, league_stats, column_headers, table_type):
+    def _make_df(self, year, league_stats, column_names):
         """
-        Makes a data frame using a dictionary's keys as column names and list of player_object.__dict__'s as data. The
-        player's unique URL is used as the data frame's index.
-
-        :param year: Season's year used to create a unique index for the player's season in the data set.
-        :param player_dicts: List of player_object.__dict__'s.
-        :param table_type: String to get column names for data frame.
-
+        :param year: Season's year.
+        :param league_stats: List where each element is a list of stats for a single player.
+        :param column_names: List used for data frame's column names.
         :return: A data frame.
         """
-        # Get data frame's columns from a relevant table dict's keys.
-        # df_columns = list(self._tables_dict[table_type]['all_columns'].keys())
-
-        df = pd.DataFrame(data=league_stats, columns=column_headers)
-        df.insert(loc=3, column='year', value=year)
-        # df['year'] = year
+        df = pd.DataFrame(data=league_stats, columns=column_names)
+        df.insert(loc=3, column='year', value=year)  # Column for current year.
 
         # Combined player_url + year acts as a unique identifier for a player's season of data.
         df['player_url'] = df['player_url'].apply(lambda x: x + str(year))
@@ -324,10 +293,10 @@ class ProRefScraper(object):
 
     def _remove_player_accolades(self, df, remove_pro_bowl, remove_all_pro):
         """
-
-        :param remove_pro_bowl:
-        :param remove_all_pro:
-        :return:
+        Removes Pro Bowl ('*') and All-Pro ('+') accolades from a player's name.
+        :param remove_pro_bowl: Boolean indicates to remove this accolade.
+        :param remove_all_pro: Boolean indicates to remove this accolade.
+        :return: No return value
         """
         if remove_pro_bowl and not remove_all_pro:
             df['player'] = df['player'].apply(lambda x: ''.join(x.split('*')))
@@ -337,15 +306,20 @@ class ProRefScraper(object):
             df['player'] = df['player'].apply(lambda x: x[:-2])
 
     def _check_table_type(self, table_type):
+        """
+        Checks for valid table types. Raises value error for invalid table.
+        :param table_type: String
+        :return: No return value
+        """
         # Only scrapes from tables in self._tables.
         if table_type.lower() not in self._tables:
-            raise KeyError("Error, make sure to specify table_type. "
+            raise ValueError("Error, make sure to specify table_type. "
                            + "Can only currently handle the following table names: "
                            + str(self._tables))
 
-# NEED FUNCTIONS TO GET RID OF PRO BOWL AND ALL PRO (*+). MAKE IT OPTIONAL.
 
 ref = ProRefScraper()
 # df = ref.get_data(2018, 2018, 'passing')
 df = ref.get_data('2005', '2006', 'passing')
+# df = ref.get_data('1969', '1969', 'fantasy')
 df.to_csv('passing05-06.csv')
